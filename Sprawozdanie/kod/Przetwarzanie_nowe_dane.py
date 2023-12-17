1d6bfcc94ae9ee00b92bd9df332803314b2133e3
Kuba <kulajakub196@gmail.com>
2023-12-17 19:12:06 +0100

    pisanie pracy ciag dalszy

---
+++ Sprawozdanie/kod/Przetwarzanie_nowe_dane.py
@@ -0,0 +1,16 @@
+path = os.getcwd()
+csv_files = glob.glob(os.path.join(path, "inputData/*.csv"))
+
+for f in csv_files:
+    dt = pd.read_csv(f)
+    for i in range(len(dataset)):
+        dt = dataset.loc[i, "Summer_Timestamps"]
+        data, time = dt.split(' ')
+        year, month, day = (int(x) for x in data.split('-'))
+        ans = datetime.date(year, month, day)
+
+        hours, minutes, null = time.split(":")
+        time = (int(hours)*60+int(minutes))/(60*24)
+
+        dane_1.loc[len(dane_1)] = {'Pora_roku': 2, 'Dzien_tygodnia': dni_tygodnia_mapa[ans.strftime(
+            "%A")], 'Czas_dnia': time, 'Przeplyw': dataset.loc[i, "Summer_Water_Consumption"]}
